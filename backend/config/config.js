06606bf5c1f5241354e7752dc145b44f72574c44
Adeoluwa Agbakosi <adeoluwaagbakosi@gmail.com>
2024-06-15 10:34:29 +0100

    chore: Update config.js with blockchain provider and contract address

--- backend/config/config.js
+++ backend/config/config.js
@@ -6,4 +6,6 @@ module.exports = {
     port: process.env.PORT || 5000,
     mongoUri: process.env.MONGO_URI,
     jwtSecret: process.env.JWT_SECRET,
+    blockchainProvider: process.env.BLOCKCHAIN_PROVIDER,
+    contractAddress: process.env.CONTRACT_ADDRESS,
 }
